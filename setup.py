1253c4ed2a2b94f360ad605bc4259e9a4a8b5f3e
James Jones <james.voip@gmail.com>
2016-10-21 02:11:58 -0400

    python package management

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Kepler',
+      version='0.1a',
+      description='Python microservice framwork',
+      author='James Jones',
+      author_email='james.voip@gmail.com',
+      url='http://outcast.github.io/kepler/',
+      packages=['kepler'],
+      )
